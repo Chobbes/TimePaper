cce6a23e27b6d760b9b121af5293e147ff0dfbb1
Calvin Beck <hobbes@ualberta.ca>
2014-09-20 21:45:43 -0600

    Simple parsing of time logs done.

---
+++ TimePaper/Main.hs
@@ -0,0 +1,51 @@
+#!/usr/bin/env runhaskell
+
+{- Copyright (C) 2014 Calvin Beck
+
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation files
+   (the "Software"), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software,
+   and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+-}
+
+import Data.List.Split
+import Data.Time.Format
+import Data.Time.LocalTime
+import System.Environment
+import System.Locale
+
+-- | Entry for time and actions.
+data TimeEntry = TimeEntry { time :: ZonedTime
+                           , actions :: [String]
+                           }
+                           deriving (Show)
+
+main = do [logFile] <- getArgs
+          timeLog <- readFile logFile
+          print (parseTimeLog timeLog)
+
+
+-- | Parse a bunch of time log lines!
+parseTimeLog :: String -> [TimeEntry]
+parseTimeLog = map parseEntry . lines
+
+-- | Parse a single line from the time log.
+parseEntry :: String -> TimeEntry
+parseEntry line = TimeEntry (readTime defaultTimeLocale "%s" time) actions
+  where (time:actions) = (filter (/= "") . splitOn " " . takeWhile (/= '[')) line
+
