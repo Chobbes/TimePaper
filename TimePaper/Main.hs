5a23b1786b1327a410924c0d49de10b4dd2c4998
Calvin Beck <hobbes@ualberta.ca>
2014-10-12 13:23:19 -0600

    Don't need to show origin.

--- TimePaper/Main.hs
+++ TimePaper/Main.hs
@@ -53,7 +53,7 @@ timeTower :: Double -> [(String, Double)] -> Diagram B R2
 timeTower length entries = vcat tower
              where sorted = sortBy (compare `on` snd) entries
                    (names, percents) = unzip sorted
-                   blockLabel n h = alignedText 0 0.5 (n ++ " -- " ++ take 4 (show h) ++ "%") # showOrigin
+                   blockLabel n h = alignedText 0 0.5 (n ++ " -- " ++ take 4 (show h) ++ "%")
                    towerBlock c n h = rect h 2 # fc c ||| (strut 0.5 # showOrigin) ||| blockLabel n h
                    towerColours = (concat . repeat) [red, yellow, green, blue, indigo, violet]
                    tower = zipWith3 towerBlock towerColours names percents
